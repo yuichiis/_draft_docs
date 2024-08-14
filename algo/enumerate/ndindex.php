3c2ddeece1c9087baa03c5bcb4e27b1baf8b0d23
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-08-15 01:36:43 +0900

    enumerate

---
+++ algo/enumerate/ndindex.php
@@ -0,0 +1,20 @@
+<?php
+
+function crawler(
+    int $depth,
+    array $indices,
+    array $shape,
+) : void
+{
+    if($depth<count($shape)) {
+        for($i=0;$i<$shape[$depth];$i++) {
+            $indices[$depth] = $i;
+            crawler($depth+1,$indices,$shape);
+        }
+        return;
+    }
+    echo "(".implode(',',$indices).")\n";
+}
+
+crawler(0,[],[4,3,2]);
+
---
+++ algo/enumerate/ndindex_loop.php
@@ -0,0 +1,28 @@
+<?php
+function crawlerLoop(array $shape) : void {
+    $depth = count($shape);
+    $indices = array_fill(0, $depth, 0);
+    $isFinished = false;
+
+    while (!$isFinished) {
+        // 現在のインデックスを出力
+        echo "(".implode(',', $indices).")\n";
+
+        // 次のインデックスへ
+        $i = $depth - 1;
+        while ($i >= 0 && $indices[$i] == $shape[$i] - 1) {
+            $indices[$i] = 0;
+            $i--;
+        }
+
+        if ($i < 0) {
+            // 全ての組み合わせを探索済み
+            $isFinished = true;
+        } else {
+            $indices[$i]++;
+        }
+    }
+}
+
+//crawlerLoop([4, 3, 2]);
+crawlerLoop([2, 3, 4]);
